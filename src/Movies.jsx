885dea3eca0315863ba0f0c6f619015c49dcb419
NemanjaMandic <nemanjans84@gmail.com>
2019-04-15 17:25:38 +0200

    added unique key

--- src/Movies.jsx
+++ src/Movies.jsx
@@ -19,15 +19,17 @@ class Movies extends Component {
                         <th>Genre</th>
                         <th>Stock</th>
                         <th>Rate</th>
+                        <th></th>
                     </tr>
                 </thead>
                 <tbody>
                     {this.state.movies.map(movie => (
-                        <tr>
+                        <tr key={movie._id}>
                           <td>{movie.title}</td>
                           <td>{movie.genre.name}</td>
                           <td>{movie.numberInStock}</td>
                           <td>{movie.dailyRentalRate}</td>
+                          <td><button className="btn btn-danger btn-sm">Delete</button></td>
                         </tr>
                     ))}
                   
