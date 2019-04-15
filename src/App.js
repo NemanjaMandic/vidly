95fde81c98314a1cfd18fb65046138c5437b3639
NemanjaMandic <nemanjans84@gmail.com>
2019-04-15 17:21:04 +0200

    nitial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Movies from './Movies';
+
+import { getMovies } from './services/fakeMovieService';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <main className="container">
+      <h1>Hej hej decace</h1>
+      <Movies />
+      </main>
     );
   }
 }
