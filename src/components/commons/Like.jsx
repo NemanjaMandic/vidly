68e08acd405b8f1ab070d63dc95d2ec44e919ce3
NemanjaMandic <nemanja.mandic@folkmatic.com>
2019-04-29 02:47:42 +0200

    changed to sfc

--- src/components/commons/Like.jsx
+++ src/components/commons/Like.jsx
@@ -1,17 +1,11 @@
-import React, { Component } from "react";
+import React from "react";
 
-class Like extends Component {
-  render() {
-    let classes = "fa fa-heart";
-    if (!this.props.liked) classes += "-o";
-    return (
-      <i
-        onClick={this.props.onClick}
-        className={classes}
-        style={{ cursor: "pointer" }}
-      />
-    );
-  }
-}
+const Like = ({ liked, onClick }) => {
+  let classes = "fa fa-heart";
+  if (!liked) classes += "-o";
+  return (
+    <i onClick={onClick} className={classes} style={{ cursor: "pointer" }} />
+  );
+};
 
 export default Like;
